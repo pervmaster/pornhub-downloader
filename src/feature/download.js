ea697de30f63258754af42caf3ab1abbd960631e
pervmaster <a930301@protonmail.com>
2020-03-05 11:44:20 -0800

    Changed multi url processing to serial from parallel

--- src/feature/download.js
+++ src/feature/download.js
@@ -2,25 +2,23 @@ const scrapy = require('../lib/scrapy');
 const log = require('../lib/log');
 const urls = process.argv.slice(2);
 
-(_ => {
-  if (!urls || !urls.length) {
-    return console.log(`Nothing to do..`);
+if (!urls || !urls.length) {
+  return console.log(`Nothing to do..`);
+}
+
+urls.reduce((download, url) => download
+.then(async _ => {
+  if (!url || !url.trim().length) {
+    return;
   }
 
-  urls.map(async url => {
-    if (!url || !url.trim().length) {
-      return;
-    }
+  if (url.includes('thumbzilla.com')) {
+    const parts = url.split('/');
+    const key = parts[parts.length - 2];
+    url = `https://www.pornhub.com/view_video.php?viewkey=${key}`;
+  }
 
-    if (url.includes('thumbzilla.com')) {
-      const parts = url.split('/');
-      const key = parts[parts.length - 2];
-      url = `https://www.pornhub.com/view_video.php?viewkey=${key}`;
-    }
-
-    const info = await scrapy.findDownloadInfo(url);
-    const result = await scrapy.downloadVideo(info);
-    log.info(result);
-    console.log('\n');
-  });
-})();
+  const info = await scrapy.findDownloadInfo(url);
+  const result = await scrapy.downloadVideo(info);
+  log.info(result);
+}), Promise.resolve(null));
