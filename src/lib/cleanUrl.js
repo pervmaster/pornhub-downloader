1a353ccf68271d10e83c11f181298d516b7c016d
pervmaster <a930301@protonmail.com>
2020-04-02 00:41:40 -0700

    Cleaned up and added premium host

--- src/lib/cleanUrl.js
+++ src/lib/cleanUrl.js
@@ -1,37 +1,47 @@
 const { URL } = require('url');
 
-const LINK_PREFIX = 'https://www.pornhub.com/view_video.php?viewkey=';
+const PH = 'pornhub.com';
+const PR = 'pornhubpremium.com';
+const TH = 'thumbzilla.com';
+
+// TODO: Find out the min and max key lengths
 const MIN_KEY_LENGTH = 8;
 const MAX_KEY_LENGTH = 15;
-// TODO: Find out the min and max key lengths
+
+const getUrl = (key, host = `www.${PH}`) => {
+  return `https://${host}/view_video.php?viewkey=${key}`;
+}
 
 const cleanUrl = url => {
-  if (!url || url.constructor !== String) {
-    throw new Error('Bad param');
-  }
+  try {
+    // Check if url is actually being passed in as just the key
+    if (url.length >= MIN_KEY_LENGTH && url.length <= MAX_KEY_LENGTH) {
+      return getUrl(url);
+    }
 
-  // Check if url is actually a key.
-  if (url.length >= MIN_KEY_LENGTH && url.length <= MAX_KEY_LENGTH) {
-    return `${LINK_PREFIX}${url}`;
-  }
-
-  // Check if url is actually a thumbzilla link
-  if (url.includes('thumbzilla.com')) {
-    const key = url.slice(url.search('/video/') + 7).split('/')[0];
-
-    return key.length ? `${LINK_PREFIX}${key}` : false;
-  }
-
-  // Check if url is pornhub format
-  if (url.includes('pornhub.com')) {
+    // Otherwise try to load as url object
     const urlObj = new URL(url);
-    const query = new URLSearchParams(urlObj.search);
-    const key = query.get('viewkey');
 
-    return key.length ? `${LINK_PREFIX}${key}` : false;
+    // Check if url is actually an alternate link
+    if (url.includes(TH)) {
+      const key = url.slice(url.search('/video/') + 7).split('/')[0];
+
+      return key.length ? getUrl(key) : false;
+    }
+
+    // Check if url is regular or premium format
+    if (url.includes(PH) || url.includes(PR)) {
+      const key = urlObj.searchParams.get('viewkey');
+
+      return key.length ? getUrl(key, urlObj.host) : false;
+    }
+
+    return false;
+  } catch (err) {
+    console.error(`[ERROR] ${err.message}`);
+
+    return false;
   }
-
-  return false;
 };
 
 module.exports = cleanUrl;
